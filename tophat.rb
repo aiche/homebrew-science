aiche/homebrew-science

commit b5d4a22d072b057d84a02bb1db411c8b2695d13b

tophat: Fails to build with clang
segment_juncs.cpp:50:17: error: expected namespace name
using namespace __gnu_cxx;

Closes #443

tophat.rb
@@ -8,6 +8,14 @@ class Tophat < Formula
   depends_on 'samtools'
   depends_on 'boost'
 
+  fails_with :clang do
+    build 500
+    cause <<-EOS.undent
+      segment_juncs.cpp:50:17: error: expected namespace name
+      using namespace __gnu_cxx;
+    EOS
+  end
+
   def install
     # Variable length arrays using non-POD element types. Initialize with length=1
     # Reported upstream via email to tophat-cufflinks@gmail.com on 28OCT2012

commit 3b360d81df71b0152757692b5598ac107576e42c

tophat: Fixed compile errors when compiling with clang.
 * the patch additionally fixes a crash introduced into tophat_reports in b5d4a22d072b057d84a02bb1db411c8b2695d13b

tophat.rb
@@ -8,19 +8,13 @@ class Tophat < Formula
   depends_on 'samtools'
   depends_on 'boost'
 
-  fails_with :clang do
-    build 500
-    cause <<-EOS.undent
-      segment_juncs.cpp:50:17: error: expected namespace name
-      using namespace __gnu_cxx;
-    EOS
+  # 1. fixes namespace issue in segment_juncs.cpp
+  # 2. fixes "Variable length arrays using non-POD element types" error in tophat_reports
+  def patches
+     DATA
   end
 
   def install
-    # Variable length arrays using non-POD element types. Initialize with length=1
-    # Reported upstream via email to tophat-cufflinks@gmail.com on 28OCT2012
-    inreplace 'src/tophat_reports.cpp', '[num_threads]', '[1]'
-
     # This can only build serially, otherwise it errors with no make target.
     ENV.deparallelize
 
@@ -36,3 +30,48 @@ class Tophat < Formula
     system "#{bin}/tophat", "--version"
   end
 end
+
+__END__
+diff --git a/src/segment_juncs.cpp b/src/segment_juncs.cpp
+index 80fb3b7..bc55db7 100644
+--- a/src/segment_juncs.cpp
++++ b/src/segment_juncs.cpp
+@@ -47,7 +47,6 @@
+
+ using namespace seqan;
+ using namespace std;
+-using namespace __gnu_cxx;
+
+ // daehwan //geo
+ //#define B_DEBUG 1
+diff --git a/src/tophat_reports.cpp b/src/tophat_reports.cpp
+index 9d0a927..a403dbe 100644
+--- a/src/tophat_reports.cpp
++++ b/src/tophat_reports.cpp
+@@ -2784,10 +2784,10 @@ for (; deletion_iter != deletions.end(); ++deletion_iter)
+ 			fprintf(stderr, "Warning: %lu small overhang junctions!\n", (long unsigned int)small_overhangs);
+ 	 */
+
+-	JunctionSet vfinal_junctions[num_threads];
+-	InsertionSet vfinal_insertions[num_threads];
+-	DeletionSet vfinal_deletions[num_threads];
+-	FusionSet vfinal_fusions[num_threads];
++	JunctionSet * vfinal_junctions = new JunctionSet[num_threads];
++	InsertionSet * vfinal_insertions = new InsertionSet[num_threads];
++	DeletionSet * vfinal_deletions = new DeletionSet[num_threads];
++	FusionSet * vfinal_fusions = new FusionSet[num_threads];
+
+ 	vector<SAlignStats> alnStats(num_threads);
+
+@@ -2931,6 +2931,11 @@ for (; deletion_iter != deletions.end(); ++deletion_iter)
+ 	}
+
+ 	fprintf(stderr, "Found %lu junctions from happy spliced reads\n", (long unsigned int)final_junctions.size());
++
++	delete[] vfinal_junctions;
++	delete[] vfinal_insertions;
++	delete[] vfinal_deletions;
++	delete[] vfinal_fusions;
+ }
+
+ void print_usage()